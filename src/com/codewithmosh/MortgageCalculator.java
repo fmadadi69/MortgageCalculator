9830e8163dc87822add05015f4892a281afb09f9
fmadadi69 <fmadadi69@gmail.com>
2025-08-04 08:33:33 +0330

    improvements done in MortgageReport & MortgageCalculator classes.

--- src/com/codewithmosh/MortgageCalculator.java
+++ src/com/codewithmosh/MortgageCalculator.java
@@ -1,39 +1,43 @@
 package com.codewithmosh;
 
 public class MortgageCalculator {
-    final byte MONTHS_IN_YEAR =12;
-    final byte PERCENT =100;
-    private final int principal;
-    private final float annualInterestRate;
-    private final byte period;
-    private final short num_payments;
-    private final float monthlyInterestRate;
+    private final static byte MONTHS_IN_YEAR =12;
+    private final static byte PERCENT =100;
 
+    private int principal;
+    private float annualInterestRate;
+    private byte period;
 
     public MortgageCalculator(int principal , float annualInterestRate , byte period ) {
         this.principal = principal;
         this.annualInterestRate = annualInterestRate;
         this.period = period;
-        this.num_payments = (short) (period * MONTHS_IN_YEAR);
-        this.monthlyInterestRate = annualInterestRate / MONTHS_IN_YEAR / PERCENT;
-
-    }
-
-    public short getNumPayments() {
-        return num_payments;
     }
 
     public double  calculateMortgage(){
         return this.principal
-                * (this.monthlyInterestRate * Math.pow(1+this.monthlyInterestRate, this.num_payments))
-                / (Math.pow(1+this.monthlyInterestRate, this.num_payments)-1);
+                * (this.getMonthlyInterestRate() * Math.pow(1+this.getMonthlyInterestRate(), this.getNumPayments()))
+                / (Math.pow(1+this.getMonthlyInterestRate(), this.getNumPayments())-1);
     }
 
     public double calculateRemainingBalance(int numberOfPaymentsMade){
         return this.principal
-                * (Math.pow(1+this.monthlyInterestRate, this.num_payments) - Math.pow(1+this.monthlyInterestRate , numberOfPaymentsMade))
-                / (Math.pow(1 + this.monthlyInterestRate, this.num_payments) - 1);
+                * (Math.pow(1+this.getMonthlyInterestRate(), this.getNumPayments()) - Math.pow(1+this.getMonthlyInterestRate() , numberOfPaymentsMade))
+                / (Math.pow(1 + this.getMonthlyInterestRate(), this.getNumPayments()) - 1);
     }
 
+    public double[] getPaymentSchedule(){
+        var paymentSchedule = new double[this.getNumPayments()] ;
+        for(short month = 1; month < paymentSchedule.length; month++)
+            paymentSchedule[month - 1] = this.calculateRemainingBalance(month);
+        return paymentSchedule;
+    }
 
+    private short getNumPayments() {
+        return (short) (period * MONTHS_IN_YEAR);
+    }
+
+    private float getMonthlyInterestRate(){
+        return annualInterestRate / MONTHS_IN_YEAR / PERCENT;
+    }
 }
--- src/com/codewithmosh/MortgageReport.java
+++ src/com/codewithmosh/MortgageReport.java
@@ -4,22 +4,28 @@ import java.text.NumberFormat;
 
 public class MortgageReport {
     private final MortgageCalculator calculator;
+    private final NumberFormat currency;
 
     public MortgageReport(MortgageCalculator calculator) {
         this.calculator = calculator;
+        currency = NumberFormat.getCurrencyInstance();
     }
     public void printMonthlyPayment(){
         System.out.println("MORTGAGE");
         System.out.println("---------");
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(this.calculator.calculateMortgage());
+        String mortgageFormatted = currency.format(this.calculator.calculateMortgage());
         System.out.println("Monthly Payments: " + mortgageFormatted + "\n");
     }
 
     public void printPaymentSchedule() {
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("------------------");
-        for (short month = 1; month <= this.calculator.getNumPayments() ; month++) {
-            System.out.printf("Month %2d: %s\n", month, NumberFormat.getCurrencyInstance().format(this.calculator.calculateRemainingBalance(month)));
+        short iterator = 1;
+        for(double payment: calculator.getPaymentSchedule()){
+            System.out.printf("Month %3d: %s\n", iterator++, currency.format(payment));
         }
+//        for (short i = 0; i <  this.calculator.getPaymentSchedule().length ; i++) {
+//            System.out.printf("Month %2d: %s\n", i+1, NumberFormat.getCurrencyInstance().format(calculator.getPaymentSchedule()[i]));
+//        }
     }
 }
